892fbffd0a06e7e2d022dbe1e7e68af42af25917
bradyrippon <brady.rippon@gmail.com>
2025-11-20 14:19:12 -0500

    Initial commit

---
+++ code/admin/libs.R
@@ -0,0 +1,25 @@
+
+# # ---------- Frequent Libraries ----------
+# ---
+# ---
+# ---
+# --- Author: Brady Rippon
+# --- Institution: WCM
+# ---
+# --- Created: 2025-07-31
+# --- Contact: brr7014@med.cornell.edu
+
+
+## load libraries ----------
+
+library(yaml)       # read in yaml files (used for metadata)
+library(here)       # shortcuts for files paths
+library(tidyverse)  # contains dplyr, readr, purrr, stringr, lubridate, ggplot
+library(magrittr)   # more pipes
+library(readxl)     # excel data imports
+library(gtsummary)  # making summary tables
+library(gt)         # making summery tables
+library(ggpubr)     # extensions for ggplot, mostly for multiple figures
+library(janitor)    # handling messy columns for data cleaning
+library(glue)       # handling string literals
+library(tidylog)    # code feedback in console
